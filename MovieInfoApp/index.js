9a7dfcd46797895d903436975a3ac74e2c5fb95b
Mansi Dhamne <mansi@Mansis-MacBook-Air.local>
2023-10-29 01:17:36 +0530

    improved overall ui/ux

--- MovieInfoApp/index.js
+++ MovieInfoApp/index.js
@@ -1,32 +1,35 @@
 const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
-const movieTitle = document.getElementById('movie-title');
-const movieGenre = document.getElementById('movie-genre');
-const movieCast = document.getElementById('movie-cast');
-const movieRating = document.getElementById('movie-rating');
-const movieReleased = document.getElementById('movie-released');
-const movieDirector = document.getElementById('movie-director');
-const moviePoster = document.getElementById('movie-poster');
+const moviePoster = document.getElementById('poster');
+const movieInfoText = document.getElementById("movieInfoText");
+const apiKey = '5b32bde5'; //replace with your API_KEY from OMDB API
+
+async function displaySearch(query) {
+    const search = `https://www.omdbapi.com/?t=${query}&apikey=${apiKey}`;
+    
+    try {
+        const response = await fetch(search);
+        const data = await response.json();
+        if(data.Response === "True"){
+            movieInfoText.innerHTML = `
+            <h2>${data.Title}</h2>
+            <p>Year: ${data.Year}</p>
+            <p>Genre: ${data.Genre}</p>
+            <p>Director: ${data.Director}</p>
+            <p>Plot: ${data.Plot}</p>
+            <p>Awards: ${data.Awards}</p>`
+            moviePoster.innerHTML = `<img src="${data.Poster}" alt="${data.Title} Poster"</img>`;
+        } else{
+            movieInfoText.innerHTML = "No results found";
+        }
+    } catch(error){
+        console.error('Error fetching data: ', error);
+    }
+}
 
 searchButton.addEventListener('click', () => {
-    // Fetch movie data from an API or your database and update the DOM elements accordingly.
-    // For example, you can use the OMDB API for this purpose.
-    const apiKey = 'adff2bf8';
     const query = searchInput.value;
-
-    // Make an API request to get movie data
-    fetch(`https://www.omdbapi.com/?t=${query}&apikey=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-            movieTitle.textContent = data.Title;
-            movieGenre.textContent = data.Genre;
-            movieCast.textContent = data.Actors;
-            movieRating.textContent = data.imdbRating;
-            movieReleased.textContent = data.Released;
-            movieDirector.textContent = data.Director;
-            moviePoster.src = data.Poster;
-        })
-        .catch(error => {
-            console.error('Error fetching data: ', error);
-        });
-});
+    if(query){
+        displaySearch(query);
+    }
+})
