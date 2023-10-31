fea1caa6475f2bb19e049a2ba97cb233e1ec7556
Varun J <vnjhaveri@gmail.com>
2023-10-31 19:59:50 +0530

    Added new data for movie info app

--- MovieInfoApp/index.js
+++ MovieInfoApp/index.js
@@ -2,7 +2,7 @@ const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 const moviePoster = document.getElementById('poster');
 const movieInfoText = document.getElementById("movieInfoText");
-const apiKey = '5b32bde5'; //replace with your API_KEY from OMDB API
+const apiKey = '5b32bde5'; 
 
 async function displaySearch(query) {
     const search = `https://www.omdbapi.com/?t=${query}&apikey=${apiKey}`;
@@ -16,8 +16,18 @@ async function displaySearch(query) {
             <p>Year: ${data.Year}</p>
             <p>Genre: ${data.Genre}</p>
             <p>Director: ${data.Director}</p>
+            <p>Actors: ${data.Actors}</p>
             <p>Plot: ${data.Plot}</p>
-            <p>Awards: ${data.Awards}</p>`
+            <p>Awards: ${data.Awards}</p>
+            <p>Runtime: ${data.Runtime}</p>
+            <p>Language: ${data.Language}</p>
+            <p>Country: ${data.Country}</p>
+            <p>Production: ${data.Production}</p>
+            <p>Box Office: ${data.BoxOffice}</p>
+            <p>DVD Release Date: ${data.DVD}</p>
+            <p>Website: ${data.Website}</p>
+            <p>Metascore: ${data.Metascore}</p>
+            <p>IMDb Rating: ${data.imdbRating}</p>`
             moviePoster.innerHTML = `<img src="${data.Poster}" alt="${data.Title} Poster"</img>`;
         } else{
             movieInfoText.innerHTML = "No results found";
@@ -33,3 +43,11 @@ searchButton.addEventListener('click', () => {
         displaySearch(query);
     }
 })
+searchInput.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+        const query = searchInput.value;
+        if(query){
+            displaySearch(query);
+        }
+    }
+});
